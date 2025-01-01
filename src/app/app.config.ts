db99cb8185719d580ad1cd0199bdb7bf1bf6e925
Sukhdev Thakur <devil51sukhdev@gmail.com>
2025-01-01 21:10:09 +0545

    WiP

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,35 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { routes } from './app-routing.module';
 import { provideClientHydration } from '@angular/platform-browser';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+
+registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideNzI18n(en_US),
+    importProvidersFrom(
+      CommonModule,
+      NzLayoutModule,
+      NzButtonModule,
+      NzFormModule,
+      NzInputModule,
+      NzSpinModule
+    ),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+  ],
 };
