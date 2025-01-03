e5c0e65d8f89964e46932568f1ff3a39c7c0c7c7
Sukhdev Thakur <devil51sukhdev@gmail.com>
2025-01-03 20:04:10 +0545

    WiP

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { PostPropertyComponent } from './components/post-property/post-property.component';
 
 const routes: Routes = [
-  {path: "dashboard", component: AdminDashboardComponent  }
+  {path: "dashboard", component: AdminDashboardComponent  },
+  {path: "property", component: PostPropertyComponent}
 ];
 
 @NgModule({
