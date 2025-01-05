0e21ec7bbbf631d2702c74cbd5bbcef880441556
Sukhdev Thakur <devil51sukhdev@gmail.com>
2025-01-05 22:03:36 +0545

    wip

--- src/app/modules/admin/services/admin.service.ts
+++ src/app/modules/admin/services/admin.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { StorageService } from '../../../auth/services/storage/storage.service';
+
+const BASIC_URL = ["http://localhost:8081"]
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  postProperty(propertyDto: any){
+return this.http.post(BASIC_URL + "/api/admin/property",propertyDto,{
+  headers: this.createAuthorizationHeader()
+})
+  }
+
+  createAuthorizationHeader(): HttpHeaders {
+    let authHeader: HttpHeaders = new HttpHeaders();
+    return authHeader.set(
+      "Authorization",
+      "Bearer" + StorageService.getToken()
+    );
+  }
 }
