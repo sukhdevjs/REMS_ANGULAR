6f8d8d185b222abc0892f7d190f452e2c71a3513
Sukhdev Thakur <devil51sukhdev@gmail.com>
2025-01-13 19:54:05 +0545

    wip

--- src/app/modules/admin/services/admin.service.ts
+++ src/app/modules/admin/services/admin.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { StorageService } from '../../../auth/services/storage/storage.service';
+import { Observable } from 'rxjs';
 
 const BASIC_URL = ["http://localhost:8081"]
 
@@ -17,11 +18,24 @@ return this.http.post(BASIC_URL + "/api/admin/property",propertyDto,{
 })
   }
 
+  getAllProperty(): Observable<any>{
+    return this.http.get(BASIC_URL + "/api/admin/list-properties", {
+      headers: this.createAuthorizationHeader()
+    })
+
+  }
+
+  deleteProperty(propertyId: number): Observable<any>{
+    return this.http.delete(BASIC_URL + "/api/admin/property/" +propertyId,{
+      headers: this.createAuthorizationHeader()
+    })
+  }
+
   createAuthorizationHeader(): HttpHeaders {
-    let authHeader: HttpHeaders = new HttpHeaders();
-    return authHeader.set(
-      "Authorization",
-      "Bearer" + StorageService.getToken()
+    let authHeaders: HttpHeaders = new HttpHeaders();
+    return authHeaders.set(
+      'Authorization',
+      'Bearer ' + StorageService.getToken()
     );
   }
 }
