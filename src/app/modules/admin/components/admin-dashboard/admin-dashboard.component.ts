b118c05aadd19bb9e8b78663146d607350f344dd
Sukhdev Thakur <devil51sukhdev@gmail.com>
2025-01-13 19:53:32 +0545

    wip

--- src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../services/admin.service';
+import { CommonModule } from '@angular/common';
+import {NzMessageService} from "ng-zorro-antd/message"
+import { RouterModule } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './admin-dashboard.component.html',
-  styleUrl: './admin-dashboard.component.scss'
+  styleUrls: ['./admin-dashboard.component.scss']
 })
 export class AdminDashboardComponent {
+  
+
+  properties: any = [];
+
+  constructor(private adminService: AdminService,
+              private message: NzMessageService
+  ) {
+    this.getAllProperty();
+  }
+
+ getAllProperty(){
+    this.adminService.getAllProperty().subscribe((res) => {
+      console.log(res);
+      res.forEach((element: any) =>{
+        element.processedImg = 'data:image/jpeg;base64,' + 
+        element.returnedImage;
+        this.properties.push(element);
+      });
+    })
+  } 
+
+  
+
+  deleteProperty(id:number){
+    this.properties = [];
+    this.adminService.deleteProperty(id).subscribe((res) =>{
+      this.message.success("Property Deleted Sucessfully", {nzDuration: 5000});
+      this.getAllProperty();
+    })
+
+  }
 
 }
