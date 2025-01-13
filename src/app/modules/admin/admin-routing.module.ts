daf19c6c086ba9784fcc7a00ccd6de876ec72fb9
Sukhdev Thakur <devil51sukhdev@gmail.com>
2025-01-13 19:51:52 +0545

    wip

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { PostPropertyComponent } from './components/post-property/post-property.component';
+import { UpdateCarComponent } from './components/update-car/update-car.component';
 
 const routes: Routes = [
   {path: "dashboard", component: AdminDashboardComponent  },
-  {path: "property", component: PostPropertyComponent}
+  {path: "property", component: PostPropertyComponent},
+  {path: "property/:id/edit", component: UpdateCarComponent}
 ];
 
 @NgModule({
